0ac26960d03d7756766dc871e18dc5cfa3fed399
seungyul1 <seungyul123@gmail.com>
2022-08-02 17:51:37 +0900

    update

--- Algorithm/Sort/Merge_Sort.c
+++ Algorithm/Sort/Merge_Sort.c
@@ -1,16 +1,10 @@
-#include <stdio.h>
-void MergeSort(int arr[],int low, int high); //분할
+//참조
+//https://m.blog.naver.com/jysaa5/221883964893
+
+//#include <stdio.h>
+
 int main() {
 
 
     return 0;
-}
-
-
-void MergeSort(int arr[],int low, int high){
-    if(high == low) return; //같으면 1개의 원소를 의미
-    int mid = (high + low)/2;
-
-    MergeSort(arr, low, mid);
-    MergeSort(arr, mid+1,high);                   
 }
