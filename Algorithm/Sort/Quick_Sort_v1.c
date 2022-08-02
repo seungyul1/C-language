4bf4068c80c99ce6a38e02c313b9fcbb122ace75
seungyul1 <seungyul123@gmail.com>
2022-08-02 16:31:36 +0900

    update

---
+++ Algorithm/Sort/Quick_Sort_v1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void Partition(int arr[], int Left, int Right);
+void Swap(int *a, int *b);
+void Print_Array(int arr[], int length);
+
+int main(){
+    //int arr[] = {5,1,6,3,4,2,7};
+    int arr[] = {5,3,7,8,4,9,1,2};
+    int length = sizeof(arr) / sizeof(int) ;
+    
+    Partition(arr,0,length-1);
+    Print_Array(arr,length);
+
+    return 0;
+}
+
+void Swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp; 
+}
+void Print_Array(int arr[], int length){
+    for(int i=0; i < length; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void Partition(int arr[], int Left, int Right){
+    int pivot = arr[(Left+Right)/2]; //idx
+    int left = Left;
+    int right = Right;
+    
+    do {
+        while (arr[left] < pivot)   //클 때까지 반복
+            left++;
+        while (arr[right] > pivot)  //작을 때 까지 반복
+            right--;
+        if(left <= right){          //
+            Swap(&arr[left], &arr[right]);
+            left++;
+            right--;
+        }
+    } while (left <= right);
+    //Print_Array(arr,8);
+    if(Left < right)
+        Partition(arr,Left,right);
+    if(Right > left)
+        Partition(arr,left,Right);
+}
---
+++ Algorithm/Sort/Quick_Sortv2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void Swap(int *a, int *b);
+void Print_Array(int arr[], int length);
+int Partition(int arr[], int Start, int End);
+void Quick_Sort(int arr[], int Start, int End);
+
+int main(){
+    int arr[] = {3,5,1,10,8,4,9,2,6,7};
+    
+    Quick_Sort(arr,0,9);
+    Print_Array(arr,10);
+    return 0;
+}
+
+void Swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+void Print_Array(int arr[], int length){
+    for(int i=0; i <length; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void Quick_Sort(int arr[], int Start, int End){
+    int Pivot_pos;
+    if(Start >= End) //원소개 1개이거나 없는 경우 아무것도 안함
+        return;
+    Pivot_pos = Partition(arr,Start,End);
+    Quick_Sort(arr,Start,Pivot_pos-1); //작은 부분
+    Quick_Sort(arr,Pivot_pos+1,End);   //큰 부분
+}
+
+
+int Partition(int arr[], int Start, int End){
+    //int pivot = arr[(Start + End)/2];
+    int pivot = arr[End];
+    int position = Start; //현재 피벗보다 작은 값이 올 위치!
+
+    for(int curr_arg=Start; curr_arg <= End; curr_arg++){
+        if(arr[curr_arg] <= pivot){
+            Swap(&arr[curr_arg], &arr[position]);
+            position++;
+        }
+    }
+    return position-1; //최종 피벗의 위치! -1해줘야함!!!!!
+}
